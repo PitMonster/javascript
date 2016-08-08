4f32dffb75bf0cb4e8b983dccb6bbaebe12a9322
raghunathtayanna <raghunath.tayanna@chargepoint.com>
2016-08-07 21:04:37 -0700

    Added Jquery replace

--- frontend-nanodegree-resume/js/resumeBuilder.js
+++ frontend-nanodegree-resume/js/resumeBuilder.js
@@ -2,7 +2,14 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
-$("#main").append(["Raghu"]);
 var name = "Ashka";
 var number = 34;
 console.log(name);
+
+var awesomeThoughts = "I am Raghu & I am Awesome" ;
+console.log(awesomeThoughts);
+
+var funthoghts = awesomeThoughts.replace("Awesome", "Fun");
+console.log(funthoghts);
+
+$("#main").append(funthoghts); 
